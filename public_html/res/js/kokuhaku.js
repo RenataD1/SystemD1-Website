9878545aec659f10bbc5d6cd0789587a01ca0525
RenataD1 <systemspace.d1@gmail.com>
2021-03-28 12:43:34 -0700

    simplified pinkMarq script

--- public_html/res/js/kokuhaku.js
+++ public_html/res/js/kokuhaku.js
@@ -1,11 +1,4 @@
-// Marquee Time
-var marquee = document.getElementById("marquee").innerHTML + " // " + Date() + " // ";
-document.getElementById("marquee").innerHTML = "";
-for(var i = 0; i < 100; i++){
-  document.getElementById("marquee").innerHTML+=marquee;
-}
-// var x = setInterval(function(){
-    // document.title = Date();
-    // document.getElementById("marquee").innerHTML += marquee;
-    // document.getElementById("m2").innerHTML = Date();
-// },6000);
+// var marquee = document.getElementById("pinkMarq").innerHTML
+// for(var i = 0; i < 100; i++){
+//   document.getElementById("pinkMarq").innerHTML+=marquee;
+// }
